300476c263559b43696968ebc386b1d3f352ad94
mcas2 <mcas2@email.com>
2024-06-03 22:29:23 +0200

    Clase por hacer

---
+++ Main.java
@@ -0,0 +1,8 @@
+// Esto debería ser el código de un programa que calcule la hipotenusa dado un tamaño de los dos lados
+// catetos de un triángulo rectángulo que introduzca el usuario.
+
+public class Main {
+	public static void main(String[] args){
+		
+	}
+}
